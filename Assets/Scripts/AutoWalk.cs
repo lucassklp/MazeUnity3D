a6195f0ec4b713b2acc5c3b4c006213963bf5278
Lucas Simas <simas.lucas@hotmail.com>
2017-12-05 08:41:52 -0300

    Suport VR

---
+++ Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class AutoWalk : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        var angles = InputTracking.GetLocalRotation(XRNode.Head);
+        transform.Rotate(0, angles.y, 0, Space.World);
+
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = transform.TransformDirection(Vector3.forward);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+}
--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -15,25 +15,24 @@ public class CameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
 
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
-        }
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
-        }
+        //transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        //if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        //}
 
     }
 }
