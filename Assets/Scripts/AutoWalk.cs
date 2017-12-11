b1b1b5dae2c8d5b126c192d47d3503bdc2ec2cb6
Lucas Simas <simas.lucas@hotmail.com>
2017-12-10 21:18:10 -0300

    Increasing speed in rotation

--- Assets/Scripts/AutoWalk.cs
+++ Assets/Scripts/AutoWalk.cs
@@ -5,7 +5,7 @@ using UnityEngine.XR;
 
 public class AutoWalk : MonoBehaviour {
 
-    public float speed = 6.0F;
+    public float speed = 5.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
@@ -13,7 +13,7 @@ public class AutoWalk : MonoBehaviour {
     void Update()
     {
         var angles = InputTracking.GetLocalRotation(XRNode.Head);
-        transform.Rotate(0, angles.y, 0, Space.World);
+        transform.Rotate(0, angles.y *= 7, 0, Space.Self);
 
         CharacterController controller = GetComponent<CharacterController>();
         moveDirection = transform.TransformDirection(Vector3.forward);
